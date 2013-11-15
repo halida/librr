88a6a08a91bec610b0c9800af18a2c499f0af048
halida <linjunhalida@gmail.com>
2013-11-15 10:23:49 +0800

    cmd interface

--- lib/librr/cmd.rb
+++ lib/librr/cmd.rb
@@ -1,8 +1,39 @@
 require 'thor'
 
 class Librr::CMD < Thor
-  def hello
-    puts 'fdsasd'
+
+  desc 'start', 'start background process'
+  def start
+    puts 'starting..'
+    # todo
+  end
+
+  desc 'stop', 'stop background process'
+  def stop
+    puts 'stopping..'
+    # todo
+  end
+
+  desc 'add DIR', 'add directory for indexing'
+  def add(dir)
+    puts "indexing: #{dir}"
+    # todo
+  end
+
+  desc 'remove DIR', 'remove directory from indexing'
+  def remove(dir)
+    puts "removing: #{dir}"
+    # todo
+  end
+
+  desc 'list', 'list all indexed directories'
+  def list
+    # todo
+  end
+
+  desc 'search STRING', 'search emacs'
+  def search(str)
+    puts "searching: #{str}"
   end
 
   def self.run!
