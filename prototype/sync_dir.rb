43125caa928511adeaf62c519936323d659f226f
halida <linjunhalida@gmail.com>
2013-12-01 11:22:24 +0800

    add sync dir script

---
+++ prototype/sync_dir.rb
@@ -0,0 +1,25 @@
+# usage:
+# ruby sync_dir.rb [DIR] [CMD]
+#
+# like:
+# ruby sync_dir.rb . "rsync -pr ./ user@xxx.com:/fda/fdas"
+
+require 'rb-fsevent'
+
+
+def main
+  fsevent = FSEvent.new
+
+  dir = ARGV[0]
+  cmd = ARGV[1]
+
+  dir = File.expand_path(dir)
+  fsevent.watch dir do |filename|
+    puts "onchange file: #{filename}"
+    system cmd
+  end
+
+  fsevent.run
+end
+
+main
